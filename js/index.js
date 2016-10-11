b13053dcd0e6affe18f55ff4171f4ccd2a2ca379
binodnme <binodnme08@gmail.com>
2016-10-11 22:13:38 +0545

    add UI and other patterns

--- js/index.js
+++ js/index.js
@@ -1,23 +1,80 @@
 /**
  * Created by kaala on 10/11/16.
  */
-var rows = 10;
-var cols = 10;
+var container = document.getElementsByClassName("container")[0];
+var rows = 150;
+var cols = 150;
 var grid = initializeGrid(rows, cols);
 
-grid[5][5] = 1;
+//glider
+// grid[5][3] = 1;
+// grid[6][4] = 1;
+// grid[4][5] = 1;
+// grid[5][5] = 1;
+// grid[6][5] = 1;
 
-grid[4][5] = 1;
-grid[6][5] = 1;
 
-console.table(grid);
+//small exploder
+
+// grid[55][54] = 1;
+// grid[54][55] = 1;
+// grid[55][55] = 1;
+// grid[56][55] = 1;
+// grid[54][56] = 1;
+// grid[56][56] = 1;
+// grid[55][57] = 1;
+
+
+//exploder
+// grid[50][50] = 1;
+// grid[50][51] = 1;
+// grid[50][52] = 1;
+// grid[50][53] = 1;
+// grid[50][54] = 1;
+//
+// grid[52][50] = 1;
+// grid[52][54] = 1;
+//
+// grid[54][50] = 1;
+// grid[54][51] = 1;
+// grid[54][52] = 1;
+// grid[54][53] = 1;
+// grid[54][54] = 1;
+
+//10 cell row
+grid[50][50] = 1;
+grid[51][50] = 1;
+grid[52][50] = 1;
+grid[53][50] = 1;
+grid[54][50] = 1;
+grid[55][50] = 1;
+grid[56][50] = 1;
+grid[57][50] = 1;
+grid[58][50] = 1;
+grid[59][50] = 1;
+
+
+
+var table;
+init();
+function init() {
+    table = createTable(rows, cols);
+    container.appendChild(table);
+    updateTable(grid);
+    var tempGrid = grid;
+
+    var intervalId = window.setInterval(function () {
+        tempGrid = scan(tempGrid);
+        updateTable(tempGrid);
+    }, 50);
+}
 
 function scan(gridIn) {
     var tempGrid = [];
     for (var i = 0; i < rows; i++) {
         var tempRow = [];
         for (var j = 0; j < cols; j++) {
-            var result = deadAliveOrReproduce(grid, i, j);
+            var result = deadAliveOrReproduce(gridIn, i, j);
             if (result == -1) tempRow.push(0);
             else if (result == 1) tempRow.push(1);
             else tempRow.push(gridIn[i][j]);
@@ -26,9 +83,6 @@ function scan(gridIn) {
     }
     return tempGrid;
 }
-
-console.table(scan(grid));
-
 
 function deadAliveOrReproduce(grid, indexX, indexY) {
     var neighbours = getNeighbours(grid, indexX, indexY);
@@ -70,4 +124,36 @@ function initializeGrid(row, col) {
         tempGrid.push(temp);
     }
     return tempGrid;
+}
+
+
+function createTable(rows, cols) {
+    var tempTable = document.createElement('table');
+
+    for (var row = 0; row < rows; row++) {
+        var tempTr = document.createElement('tr');
+        for (var col = 0; col < cols; col++) {
+            var tempTd = document.createElement('td');
+
+            tempTr.appendChild(tempTd);
+        }
+
+        tempTable.appendChild(tempTr);
+    }
+
+    return tempTable;
+}
+
+function updateTable(refGrid) {
+    var tableRow = table.children;
+    for (var row = 0; row < tableRow.length; row++) {
+        var td = tableRow[row].getElementsByTagName('td');
+        for (var i in td) {
+            if (refGrid[row][i] == 1) {
+                td[i].className = "alive";
+            } else {
+                td[i].className = "";
+            }
+        }
+    }
 }
